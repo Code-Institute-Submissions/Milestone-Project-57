cea6a1889891e3726dae88e94ec4e27501f04a00
Aman Gurung <creates.creation@gmail.com>
2020-05-18 22:36:20 +0000

    creating variables option1 and options2 and  changing variable name ranNum to randomID

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
 let correctPokemon;
- const start = async () => {
-    //create a random number for correct answer
-    const ranNum = Math.floor(Math.random() * 151)+1;
-    // call the pokemon api for the data
-    const url = `https://pokeapi.co/api/v2/pokemon/${ranNum}`;
-    const response =  await fetch(url);
+let correctPokemon2;
+
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+
+//creating random number for id and 151 is first generation pokemon
+
+let randomID = Math.floor(Math.random() * 151) + 1;
+
+const start = async () => {
+  // call the pokemon api for the data
+  const url = `https://pokeapi.co/api/v2/pokemon/${randomID}`;
+  const response = await fetch(url);
+  const json = await response.json();
+  correctPokemon = {
+    name: json.name,
+    id: json.id,
+    image: json.sprites.front_default,
+  };
+
+  // join the pokemon image data
+  document.getElementById("correct-image").src = correctPokemon.image;
+
+  // Adding to html
+
+  option1.innerHTML = `A. ${correctPokemon.name}`;
+
+  //creating another random pokemon for option 2
+
+  const start2 = async () => {
+    let randomID = Math.floor(Math.random() * 151) + 1;
+    const url = `https://pokeapi.co/api/v2/pokemon/${randomID}`;
+    const response = await fetch(url);
     const json = await response.json();
-    correctPokemon = {
-            name: json.name,
-            id: json.id,
-            image: json.sprites.front_default
-        };
-        console.log(correctPokemon);
-    // join the pokemon image data
- document.getElementById('correct-image').src = correctPokemon.image;
-    // create other 3 name data random for options
+    correctPokemon2 = {
+      name: json.name,
+    };
 
-    // call api for the data
+    // Adding to html
 
-    //join the pokemon name data
-
-    //
-
-}
+    option2.innerHTML = `B. ${correctPokemon2.name}`;
+  };
+  start2();
+};
 start();
+
+option1.onclick = function () {
+  option1.style.backgroundColor = "rgba(30, 255, 0, 0.315)";
+  window.location = "index.html";
+};
+option2.onclick = function () {
+  option2.style.backgroundColor = "rgba(255, 0, 0, 0.315)";
+  window.location = "index.html";
+};
